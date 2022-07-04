8b46bb556e0231cca3d54204c59dede287e4226c
Adrian Fadiga <adrianmfadiga@gmail.com>
2022-07-04 13:48:14 -0300

    feat: implementada controller da tasks

--- back-end/src/controller/tasksController.ts
+++ back-end/src/controller/tasksController.ts
@@ -6,9 +6,8 @@ export default {
     async getByUserId(req: IRequestWithAdmin, res: Response, next: NextFunction) {
         try {
             const { id } = req;
-            const tasks = await tasksService.getByUserId(id);
+            const tasks = await tasksService.getByUserId(id as number);
             return res.status(200).json(tasks);
-
         } catch(err) {
             next(err);
         }
