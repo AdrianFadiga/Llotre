67b33106536c044f17f5e62cf0e10df8c2088e3b
Adrian Fadiga <adrianmfadiga@gmail.com>
2022-07-04 12:55:34 -0300

    feat: criada função getByUserId na camada controller

--- back-end/src/controller/tasksController.ts
+++ back-end/src/controller/tasksController.ts
@@ -1,8 +1,16 @@
-import { Response } from 'express';
+import { NextFunction, Response } from 'express';
 import IRequestWithAdmin from '../interfaces/IRequestWithAdmin';
+import tasksService from '../service/TasksService';
 
 export default {
-    async getUserTasks(req: IRequestWithAdmin, res: Response) {
-        const { admin } = id;
+    async getByUserId(req: IRequestWithAdmin, res: Response, next: NextFunction) {
+        try {
+            const { id } = req;
+            const tasks = await tasksService.getByUserId(id);
+            return res.status(200).json(tasks);
+
+        } catch(err) {
+            next(err);
+        }
     }
 };
