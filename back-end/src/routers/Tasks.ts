4bca32e9bee99d684c0a0e265464c02b5a7e7b40
Adrian Fadiga <adrianmfadiga@gmail.com>
2022-07-04 12:50:27 -0300

    feat: implementada rota Tasks

--- back-end/src/routers/Tasks.ts
+++ back-end/src/routers/Tasks.ts
@@ -1,6 +1,10 @@
 import express from 'express';
+import tasksController from '../controller/tasksController';
+import validateJWT from '../auth/validateJWT';
 
 const router = express.Router();
+
+router.get('/', validateJWT, tasksController.getUserTasks);
 
 export default router;
 
